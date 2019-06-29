d0a1ac9dbbbeac960070a6e8d67175fed919ef9c
Mafe Serna <mafesernaarboleda@gmail.com>
2019-06-28 20:44:35 -0500

    deleted dependecies

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import VueFeather from 'vue-feather'
 
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.use(VueFeather);
-  Vue.component('vue-feather', VueFeather)
   Vue.component('Layout', DefaultLayout)
 }
